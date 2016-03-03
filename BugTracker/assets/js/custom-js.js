56177f10e61b95fa97f922c08976cfb42f095aa9
AlexSchechter <a.schechter@outlook.com>
2016-03-02 22:01:05 -0500

    Added Layout and Implemented Basic Login & Signup

---
+++ BugTracker/assets/js/custom-js.js
@@ -0,0 +1,31 @@
+﻿$(".edit-profile-button").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#edit-profile-section").offset().top - 60
+    }, 200);
+})
+
+$(function () {
+    $('#container').highcharts({
+        data: {
+            table: 'datatable'
+        },
+        chart: {
+            type: 'column'
+        },
+        title: {
+           text: null
+        },
+        yAxis: {
+            allowDecimals: false,
+            title: {
+                text: '$'
+            }
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.series.name + '</b><br/>' +
+                    this.point.y + ' ' + this.point.name.toLowerCase();
+            }
+        }
+    });
+});
